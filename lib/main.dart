eff3a71cc229c45727f493702bdf8792dfa3736d
don-k-jacob <donkjacob.business@gmail.com>
2020-06-06 18:32:34 +0530

    updates

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData.dark().copyWith(
           accentColor: Color(0xffb31217)
